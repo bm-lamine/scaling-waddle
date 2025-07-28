c5314ce6bd59d98367f288c50a3d7fef73c3f075
bm-lamine <bennouioua.mohammed.lamine@gmail.com>
2025-07-28 16:14:12 +0100

    feat: csrf and cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { csrf } from "hono/csrf";
 
 import authRouter from "./services/auth/auth.router";
 
 const app = new Hono();
+
+app.use(csrf());
+app.use(cors());
 
 app.route("/v1/auth", authRouter);
 app.get("/", (c) => c.text("Hello Hono!"));
