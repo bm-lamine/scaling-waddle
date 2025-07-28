d67b9a19d2b2239a39d2654cd6f3c23a23e626a2
bm-lamine <bennouioua.mohammed.lamine@gmail.com>
2025-07-28 23:59:21 +0100

    feat: no more web app feat: yes for cotainers

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -4,33 +4,16 @@ export const userTable = utils.create("users", (db) => ({
   id: db.varchar().$defaultFn(utils.id).primaryKey(),
   email: db.varchar().notNull().unique(),
   password_hash: db.varchar().notNull(),
+  // timestamps
   created_at: db
     .timestamp({ mode: "date", withTimezone: true })
     .$defaultFn(utils.now)
     .notNull(),
-}));
-
-export const sessionTable = utils.create("sessions", (db) => ({
-  id: db.varchar().$defaultFn(utils.id).primaryKey(),
-  user_id: db
-    .varchar()
-    .notNull()
-    .references(() => userTable.id),
-  ip_address: db.inet(),
-  user_agent: db.text(),
-  is_valid: db.boolean().default(true).notNull(),
-  invalidated_at: db.timestamp({ mode: "date", withTimezone: true }),
-  last_verified_at: db
+  updated_at: db
     .timestamp({ mode: "date", withTimezone: true })
-    .$defaultFn(utils.now)
-    .notNull(),
-  created_at: db
-    .timestamp({ mode: "date", withTimezone: true })
-    .$defaultFn(utils.now)
-    .notNull(),
+    .$onUpdateFn(utils.now),
 }));
 
 export const schema = {
   users: userTable,
-  sessions: sessionTable,
 } as const;
