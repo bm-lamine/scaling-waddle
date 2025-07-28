525927b6b1da5c10f54cb032a12596782c72f65d
bm-lamine <bennouioua.mohammed.lamine@gmail.com>
2025-07-28 22:10:44 +0100

    fix: cleanup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,14 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-
-import { showRoutes } from "hono/dev";
-import authRouter from "./routers/auth/auth.router";
+import * as dev from "hono/dev";
 
 const app = new Hono();
 
-// app.use(csrf()); // Turn on for production
 app.use(cors({ credentials: true, origin: ["*"] }));
 
-app.route("/v1/auth", authRouter);
 app.get("/", (c) => c.text("Hello Hono!"));
 
-showRoutes(app);
+dev.showRoutes(app);
 
 export default {
   port: 4321,
